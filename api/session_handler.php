836a9c1bf58ff577fcc4bfe6ea60fda28c07326d
matt <matmaniego@gmail.com>
2026-08-19 16:18:11 +0800

    fix: allow full DB environment variable configuration (DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASSWORD)

--- api/session_handler.php
+++ api/session_handler.php
@@ -5,12 +5,15 @@ class DatabaseSessionHandler implements SessionHandlerInterface {
     public function open($savePath, $sessionName): bool {
         $_cfg = __DIR__ . '/db_config.php';
         if (file_exists($_cfg)) { require_once $_cfg; }
-        $sname   = defined('_DB_HOST') ? _DB_HOST : 'mysql-35594af1-reapquizon-ff22.h.aivencloud.com';
-        $unmae   = defined('_DB_USER') ? _DB_USER : 'avnadmin';
+        
+        $sname    = defined('_DB_HOST') ? _DB_HOST : ($_ENV['DB_HOST'] ?? $_SERVER['DB_HOST'] ?? getenv('DB_HOST') ?: 'mysql-35594af1-reapquizon-ff22.h.aivencloud.com');
+        $unmae    = defined('_DB_USER') ? _DB_USER : ($_ENV['DB_USER'] ?? $_SERVER['DB_USER'] ?? getenv('DB_USER') ?: 'avnadmin');
         $password = defined('_DB_PASS') ? _DB_PASS : ($_ENV['DB_PASSWORD'] ?? $_SERVER['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?: null);
+        $db_name  = defined('_DB_NAME') ? _DB_NAME : ($_ENV['DB_NAME'] ?? $_SERVER['DB_NAME'] ?? getenv('DB_NAME') ?: 'defaultdb');
+        $port     = defined('_DB_PORT') ? _DB_PORT : ($_ENV['DB_PORT'] ?? $_SERVER['DB_PORT'] ?? getenv('DB_PORT') ?: '22331');
+        
         if (!$password) { return false; }
-        $db_name = defined('_DB_NAME') ? _DB_NAME : 'defaultdb';
-        $port    = defined('_DB_PORT') ? _DB_PORT : '22331';
+        
         $this->db = mysqli_init();
         mysqli_options($this->db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
         mysqli_ssl_set($this->db, NULL, NULL, NULL, NULL, NULL);
